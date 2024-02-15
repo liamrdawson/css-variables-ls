3e3b0547158d5e65174645d263a627f692d015b2
Liam Dawson <liamrdawson@gmail.com>
2024-02-15 23:13:04 +0000

    Ignore lib pt2.

--- lib/log.d.ts
+++
@@ -1,5 +0,0 @@
-declare function write(message: object | unknown): void;
-declare const _default: {
-    write: typeof write;
-};
-export default _default;
--- lib/server.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function createServerConnection(): import("vscode-languageserver/node").Connection;
